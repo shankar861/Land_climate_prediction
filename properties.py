e75c4bdfa3ed879128508134d531721a32887091
shankar861 <sivak017@umn.edu>
2018-05-18 13:45:24 -0500

    First commit

---
+++ properties.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Parameters for preprocessing
+
+
+
+# Parameters for linear model - Principal component regression
+
+
+# Parameters for linear model - Lasso
+
+
+
+# Parameters for linear model - Adaptive Lasso
+
+
+# Parameters for non linear model - MLP
+
+
+# Parameters for non linear model - Convolutional neural networks
+
+
+# Parameters for non linear model - transfer learning 
+
