4e7fd39fda65b651420e922ff91697fc50087047
Sijie He <meganhe893@gmail.com>
2018-05-21 17:31:53 -0500

    hyper parameter

--- properties.py
+++ properties.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 # Parameters for preprocessing
 
-
+Train_size=120
+Test_size=10
+Step_size=10
 
 # Parameters for linear model - Principal component regression
 
 
 # Parameters for linear model - Lasso
 
-
+Lambda_start=0.01
+Lambda_end=0.5
+num_lambda=50
 
 # Parameters for linear model - Adaptive Lasso
 
